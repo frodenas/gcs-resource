532138e27b26e26d22dd2281ca1df006532a5302
Ferran Rodenas <frodenas@gmail.com>
2018-02-01 20:15:44 +0100

    Update integration-tests CI script with latest Makefile changes

--- ci/tasks/integration-tests.sh
+++ ci/tasks/integration-tests.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+: ${GCS_RESOURCE_JSON_KEY:?}
+: ${GCS_RESOURCE_BUCKET_NAME:?}
+: ${GCS_RESOURCE_VERSIONED_BUCKET_NAME:?}
+
 export GOPATH=$PWD/go
 export PATH=$GOPATH/bin:$PATH
 
 cd $GOPATH/src/github.com/frodenas/gcs-resource
-make get-deps
 make integration-tests
